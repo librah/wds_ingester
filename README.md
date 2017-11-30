a077ceb148c97001597b561762b3bb1f5cf36527
librah <huang.libra@gmail.com>
2017-11-30 16:29:32 +0800

    Enable to ingest documents/files from local file system

--- README.md
+++ README.md
@@ -1,17 +1,15 @@
 # IBM Watson Discovery Service (WDS) - Document Ingester
-WDS Ingester read documents from the input queue, ingest the documents to IBM WDS,
-and put the WDS enriched response documents to the response queue.
+WDS Ingester read documents from `input`, ingest the documents to IBM WDS,
+and put the WDS enriched response documents to `output`.
 
-
-## Run Rabbit MQ
-```console
-$ docker run -d --hostname my-rabbit --name my-rabbit -p 15672:15672 -e RABBITMQ_DEFAULT_USER=<user> -e RABBITMQ_DEFAULT_PASS=<password> rabbitmq:3-management
-```
-
-Visit the admin interface at: `http://my-rabbit:15672`
+Currently, the `input` and `output` can be a directory.
+In the future, `input` and `output` can be a RabbitMQ.
 
 ## Run WDS Ingester
+
+
 ```console
-$ docker run -d --rm --link=my-rabbit:rabbitmq -e WDS_USER=<username> -e WDS_PASSWD=<password> -e WDS_COLLECTION_ID=<collection_id> -e WDS_ENV_ID=<env_id> -e WDS_VERSION=<version> -e RABBIT_MQ_USER=<user> -e RABBIT_MQ_PASSWD=<passwd> --name my-ingester librah/wds_ingester 
+$ 
+$ docker run -d --rm -e WDS_USER=<username> -e WDS_PASSWD=<password> -e WDS_COLLECTION_ID=<collection_id> -e WDS_ENV_ID=<env_id> -e WDS_VERSION=<version> -e RABBIT_MQ_USER=<user> -e RABBIT_MQ_PASSWD=<passwd> --name my-ingester librah/wds_ingester 
 ```
 
--- wds/ingester.py
+++ wds/ingester.py
@@ -1,3 +1,5 @@
+import argparse
+import codecs
 import json
 import logging
 import os
@@ -5,140 +7,160 @@ from StringIO import StringIO
 import sys
 import time
 
-import pika
-
 from watson_developer_cloud import DiscoveryV1
 
+# Use the built-in version of scandir/walk if possible, otherwise
+# use the scandir module version
+try:
+    from os import scandir
+except ImportError:
+    from scandir import scandir
 
 #
 #  Set the default logging
 #
-logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger('ingester')
 
-
 #
-# Checking the required environment variables and initialize them
+# Evil global variables used in this code
 #
-for required_env in ('WDS_USER', 'WDS_PASSWD', 'WDS_COLLECTION_ID', 'WDS_ENV_ID', 'WDS_VERSION'):
-    if required_env not in os.environ:
-        print 'Missing required env variable "%s"' % required_env
-        sys.exit(-1)
-
-WDS_COLLECTION_ID = os.getenv('WDS_COLLECTION_ID')
-WDS_ENV_ID = os.getenv('WDS_ENV_ID')
-
-discovery_option = dict()
-discovery_option['username'] = os.getenv('WDS_USER')
-discovery_option['password'] = os.getenv('WDS_PASSWD')
-if 'WDS_URL' in os.environ:
-    discovery_option['url'] = os.getenv('WDS_URL')
+ingest_input = None  # where to get the document for ingestion
+ingest_output = None  # where to store the ingestion result
 
 
-#
-# Creating the WDS wrapper
-#
-discovery = DiscoveryV1(os.getenv('WDS_VERSION'), **discovery_option)
+class WDSIngester:
+    def __init__(self, user, password, env_id, collection_id, version):
+        discovery_option = {
+            'username': user,
+            'password': password
+        }
+        self.discovery = DiscoveryV1(version, **discovery_option)
+        self.env_id = env_id
+        self.collection_id = collection_id
+        self.version = version
+
+        collection = self.discovery.get_collection(self.env_id, self.collection_id)
+        logger.info('Creating WDSIngester to WDS collection: %s (%s)', collection['name'], collection['collection_id'])
+
+    def update_document(self, doc_id, doc):
+        io = StringIO()
+        json.dump(doc, io)
+        io.seek(0)
+        logger.info('Ingesting document %s', doc_id)
+        wds_result = self.discovery.update_document(self.env_id, self.collection_id, doc_id, file_data=io, mime_type='application/json')
+        logger.info('WDS response: %s', wds_result)
+        assert 'processing' in wds_result['status']
+        return wds_result
+
+    def delete_document(self, doc_id):
+        logger.info('Deleting document %s', doc_id)
+        wds_result = self.discovery.delete_document(self.env_id, self.collection_id, doc_id)
+        logger.info('WDS response: %s', wds_result)
+        return wds_result
+
+    def get_document(self, doc_id, poll_delay=2):
+        """
+        :param doc_id:
+        :param poll_delay: wait poll_delay seconds before next pull
+        :return:
+            Exception will be thrown by the underlying Watson Discovery API if doc_id is not found
+        """
+        logger.info('Getting document %s', doc_id)
+        while True:
+            wds_result = self.discovery.get_document(self.env_id, self.collection_id, doc_id)
+            logger.info('WDS response: %s', wds_result)
+            if 'available' in wds_result['status']:
+                # pull the document
+                qopts = {
+                    'filter': '(id::"%s")' % doc_id
+                }
+                wds_result = self.discovery.query(self.env_id, self.collection_id, qopts)
+                assert wds_result['matching_results'] == 1
+                return wds_result['results'][0]
+            elif 'processing' in wds_result['status']:
+                time.sleep(poll_delay)
+            else:
+                logger.error('Unexpected status string: %s', wds_result['status'])
+                raise Exception('Unexpected status')
 
 
-#
-# Creating the RabbitMQ wrapper
-#
-RABBIT_MQ_HOST = os.getenv('RABBIT_MQ_HOST') if 'RABBIT_MQ_HOST' in os.environ else 'rabbitmq'
-RABBIT_MQ_IN_QUEUE = os.getenv('RABBIT_MQ_IN_QUEUE') if 'RABBIT_MQ_IN_QUEUE' in os.environ else 'w4cs_input_queue'
-RABBIT_MQ_OUT_QUEUE = os.getenv('RABBIT_MQ_OUT_QUEUE') if 'RABBIT_MQ_OUT_QUEUE' in os.environ else 'w4cs_output_queue'
-RABBIT_MQ_USER = os.getenv('RABBIT_MQ_USER') if 'RABBIT_MQ_USER' in os.environ else None
-RABBIT_MQ_PASSWD = os.getenv('RABBIT_MQ_PASSWD') if 'RABBIT_MQ_PASSWD' in os.environ else None
-
-
-def update_document(doc):
-    io = StringIO()
-    json.dump(doc, io)
-    io.seek(0)
-    if 'id' in doc:
-        logger.info('Receive document and ingesting %s', doc['id'])
-        wds_result = discovery.update_document(WDS_ENV_ID, WDS_COLLECTION_ID, doc['DOCNO'], file_data=io, mime_type='application/json')
-    else:
-        logger.info('Receive document and ingesting')
-        wds_result = discovery.add_document(WDS_ENV_ID, WDS_COLLECTION_ID, file_data=io, mime_type='application/json')
-    logger.info('WDS response: %s', wds_result)
-    return wds_result
-
-
-def delete_document(id):
-    logger.info('Delete document %s', id)
-    wds_result = discovery.delete_document(WDS_ENV_ID, WDS_COLLECTION_ID, id)
-    logger.info('WDS response: %s', wds_result)
-    return wds_result
-
-
-def get_document(id):
-    qopts = {
-        'filter': '(id::"%s")' % id
-    }
-    result = discovery.query(WDS_ENV_ID, WDS_COLLECTION_ID, qopts)
-    if result['matching_results'] != 1:
-        logger.info('%d matches found for docid %s', result['matching_results'], id)
-        return None
-    else:
-        return result['results'][0]
-
-
-def callback(ch, method, properties, body):
-    try:
-        request = json.loads(body, encoding='utf-8')
-
-        method = request['method']
-        body = request['body']
-        if method == 'update':
-            wds_result = update_document(body)
-            doc_id = wds_result['document_id']
-            time.sleep(1)
-            while True:
-                doc_info = discovery.get_document(WDS_ENV_ID, WDS_COLLECTION_ID, doc_id)
-                logger.info('Checking document %s status: %s', doc_id, doc_info)
-                doc_status = doc_info['status']
-                if doc_status == 'available' or doc_status == 'available with notices':
-                    logger.info('Pull the enriched result from WDS and put to output queue')
-                    enriched_doc = get_document(doc_id)
-                    mq_out_channel.basic_publish(exchange='', routing_key=RABBIT_MQ_OUT_QUEUE, body=enriched_doc, properties=pika.BasicProperties(delivery_mode=2))
-                    break
-                elif doc_status == 'processing':
-                    time.sleep(5)
-                    continue
-                else:
-                    raise 'Unexpected get_document status error'
-        elif method == 'delete':
-            delete_document(body)
-            mq_out_channel.basic_publish(exchange='', routing_key=RABBIT_MQ_OUT_QUEUE, body=request, properties=pika.BasicProperties(delivery_mode=2))
+def scan_left_over_files(path):
+    """
+    Recursively yield file DirEntry objects for the given `path` directory.
+    :return: DirEntry
+    """
+    for entry in scandir(path):
+        if entry.is_dir(follow_symlinks=False):
+            for child in scan_left_over_files(entry.path):
+                yield child
         else:
-            raise 'Unknown operation %s' % method
-    except:
-        logger.exception('Unabe to process received document, move document to error queue: %s', body)
-    finally:
-        mq_in_channel.basic_ack(delivery_tag=method.delivery_tag)
+            yield entry
+
+
+def parse_args():
+    """
+    Parse the command-line arguments and save the parsing result to the global (evil) variables
+    :return: None
+    """
+    global logger
+
+    parser = argparse.ArgumentParser(description='Ingest files to Watson Discovery Service (WDS)')
+
+    parser.add_argument('--input', required=True, help='The input channel, file://directory, others')
+    parser.add_argument('--d', required=False, default=False, action='store_true', help='Run in background. Default No')
+    parser.add_argument('--output', required=False, help='Pull WDS enriched result to the output')
+
+    args = parser.parse_args()
+
+    global ingest_input
+    ingest_input = os.path.abspath(os.path.expanduser(args.input))
+
+    global ingest_output
+    ingest_output = os.path.abspath(os.path.expanduser(args.output))
+
+
+def main():
+    wds_ingester = WDSIngester(os.getenv('WDS_USER'),
+                               os.getenv('WDS_PASSWD'),
+                               os.getenv('WDS_ENV_ID'),
+                               os.getenv('WDS_COLLECTION_ID'),
+                               os.getenv('WDS_VERSION'))
+    for entry in scan_left_over_files(ingest_input):
+        if entry.is_file():
+            try:
+                file_path = entry.path
+                with open(file_path, 'r') as f:
+                    document = json.loads(f.read().decode('utf-8-sig'))
+                    document_id = document['id']
+
+                wds_ingester.update_document(document_id, document)
+
+                if ingest_output:
+                    enriched_document = wds_ingester.get_document(document_id)
+                    output_filename = os.path.join(ingest_output, os.path.basename(file_path))
+                    logger.info('Writing %s', output_filename)
+                    with codecs.open(output_filename, mode='w', encoding='utf-8') as f:
+                        f.write(json.dumps(enriched_document, ensure_ascii=False, indent=2))
+            except:
+                logger.exception('Error when processing %s' % entry)
 
 
 #
 # Main program
 #
 if __name__ == '__main__':
-    collection = discovery.get_collection(WDS_ENV_ID, WDS_COLLECTION_ID)
-    logger.info('Ingesting documents to WDS collection: %s (%s)', collection['name'], collection['collection_id'])
+    logging.basicConfig(level=logging.INFO, format='%(asctime)-15s %(message)s')
 
-    mq_credential = pika.PlainCredentials(RABBIT_MQ_USER, RABBIT_MQ_PASSWD)
-    mq_conn = pika.BlockingConnection(pika.ConnectionParameters(host=RABBIT_MQ_HOST, credentials=mq_credential))
+    #
+    # Checking the required environment variables and initialize them
+    #
+    for required_env in ('WDS_USER', 'WDS_PASSWD', 'WDS_COLLECTION_ID', 'WDS_ENV_ID', 'WDS_VERSION'):
+        if required_env not in os.environ:
+            print 'Missing required env variable "%s"' % required_env
+            sys.exit(-1)
 
-    # create channel for putting output
-    mq_out_channel = mq_conn.channel()
-    mq_out_channel.queue_declare(queue=RABBIT_MQ_OUT_QUEUE, durable=True)
+    parse_args()
 
-    # create channel for receiving input
-    mq_in_channel = mq_conn.channel()
+    main()
 
-    mq_in_channel.queue_declare(queue=RABBIT_MQ_IN_QUEUE, durable=True)
-    mq_in_channel.basic_qos(prefetch_count=1)
-    mq_in_channel.basic_consume(callback, queue=RABBIT_MQ_IN_QUEUE)
-
-    logger.info('Monitoring documents from %s at %s', RABBIT_MQ_IN_QUEUE, RABBIT_MQ_HOST)
-    mq_in_channel.start_consuming()
+    logger.info('Done. Exiting')
